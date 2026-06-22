ffa4c737898c46bbb8e3eda2999da68548f839aa
tmahicharan <tmahicharan@gmail.com>
2026-06-22 12:03:59 +0530

    Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -27,7 +27,7 @@ VALIDATE() {
     fi
 }
 
-cp mongo.repo /etc/repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding mongo repo"
 
 dnf install mongodb-org -y &>> $LOG_FILE
