d8dddf452cc31ffb6d4df626fd062672d7085d21
tmahicharan <tmahicharan@gmail.com>
2026-06-22 12:09:54 +0530

    REPO

--- servers.sh
+++ servers.sh
@@ -8,13 +8,13 @@ DOMAIN_NAME="mahidevops.fun"
 for instance in $@
 do
    #launching server and generating instance id
-   INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+   INSTANCE_ID= $(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
    if [ $instance != "frontend" ]; then
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    IP= $(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     RECORD_NAME="$instance.$DOMAIN_NAME"
    else
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    IP= $(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     RECORD_NAME="$DOMAIN_NAME"
    fi
    echo " $instance = $IP"
